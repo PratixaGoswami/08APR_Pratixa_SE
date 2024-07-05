87470ef067982c8e8df61e08adcb7851fa2960cc
PratixaGoswami <167281022+PratixaGoswami@users.noreply.github.com>
2024-07-05 15:55:26 +0530

    Update Q-35-filecompare.cpp

--- Module-3/Q-35-filecompare.cpp
+++ Module-3/Q-35-filecompare.cpp
@@ -5,14 +5,14 @@ main()
 {
 	FILE *fl;
 	char str[100];
-	fl=fopen("File1.txt","r");
+	fl=fopen("a.txt","r");
 	fscanf(fl,"%s",str);
 	printf("%s",str);
 	fclose(fl);
 	
 	FILE *fl2;
 	char str2[100];
-	fl2=fopen("File2.txt","r");
+	fl2=fopen("b.txt","r");
 	fscanf(fl2,"\n%s",str2);
 	printf("%s",str2);
     
